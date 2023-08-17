058ff0a9362aa630260f84a5af040ae4e69fd5d8
Martin Correa <martincorrea792@gmail.com>
2023-08-17 19:25:04 -0300

    some task

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application
+
+- Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ”, followed by the value of the text variable
+/python/<text>: display “Python ”, followed by the value of the text variable
+" """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ display string """
+    return "Hello HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display string """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def func(text):
+    """ display c + text """
+    text = text.replace('_', ' ')
+    return f"c {text}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def ptyon(text='is_cool'):
+    """ display  """
+    text = text.replace('_', ' ')
+    return f"python {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application
+"""
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ display string """
+    return "Hello HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display string """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def func(text):
+    """ display c + text """
+    text = text.replace('_', ' ')
+    return f"c {text}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def ptyon(text='is_cool'):
+    """ display  """
+    text = text.replace('_', ' ')
+    return f"python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def funcion(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def funct(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
