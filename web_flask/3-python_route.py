fe65ea0450ce6c4c49137514599b9a99a2a42317
Martin Correa <martincorrea792@gmail.com>
2023-08-17 19:27:36 -0300

    fixed lowercase

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -37,7 +37,7 @@ def func(text):
 def ptyon(text='is_cool'):
     """ display  """
     text = text.replace('_', ' ')
-    return f"python {text}"
+    return f"Python {text}"
 
 
 if __name__ == '__main__':
